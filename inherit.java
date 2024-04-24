748ba5e7c7c226fc117e9a62a9f59559807bad2c
vilasbhaskarpathade <2022bit011@sggs.ac.in>
2024-04-24 20:50:01 +0530

    Inheritance Concept

---
+++ inherit.java
@@ -0,0 +1,20 @@
+
+
+import java.io.*;
+
+class Employee {
+	int salary = 60000;
+}
+
+
+class Engineer extends Employee {
+	int benefits = 10000;
+}
+
+class inherit {
+	public static void main(String args[])
+	{
+		Engineer E1 = new Engineer();
+		System.out.println("Salary : " + E1.salary+ "\nBenefits : " + E1.benefits);
+	}
+}
