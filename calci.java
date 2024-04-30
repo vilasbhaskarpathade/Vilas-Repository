9ca09d27f1ac2b5d209819ab5740de5d057a8c15
vilasbhaskarpathade <2022bit011@sggs.ac.in>
2024-04-30 21:24:52 +0530

    simple calci

--- calci.java
+++ calci.java
@@ -1,6 +1,38 @@
-class calci{
-    public static void main(String []args)
-    {
-        Scanner in = new Scanner(System.in);
+import java.util.Scanner;
+
+public class calci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double num1, num2, result;
+        char operator;
+
+        System.out.print("Enter first number: ");
+        num1 = input.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = input.nextDouble();
+
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+                return;
+            }
+        } else {
+            System.out.println("Error: Invalid operator.");
+            return;
+        }
+
+        System.out.println("Result: " + result);
     }
 }
